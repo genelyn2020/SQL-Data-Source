cf2205eb8c49c1dee33436a4cc3dd0a7b5e00d05
genelyn2020 <72040803+genelyn2020@users.noreply.github.com>
2020-12-11 17:44:46 +0800

    Add files via upload

---
+++ Insert Multiple Rows.sql
@@ -0,0 +1,1 @@
+INSERT INTO `guests`(`guest_id`, `name`) VALUES (7,'Bianca'), (8,'Ava joy'), (9,'Jean'), (10,'Krishna')
---
+++ alter table.sql
@@ -0,0 +1,3 @@
+alter table guests
+add age INT (55) ,
+add country varchar(255);
