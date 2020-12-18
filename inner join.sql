fdbeb522e74a80988e363aebc2fcdb3b284df53d
genelyn2020 <72040803+genelyn2020@users.noreply.github.com>
2020-12-18 22:12:03 +0800

    SQL INNER JOIN

---
+++ inner join.sql
@@ -0,0 +1,11 @@
+select 
+    c.name country,
+    r.name region,
+    t.name continent,
+    area
+from countries c
+inner join regions r 
+    using (region_id)
+inner join continents t 
+    using (continent_id)
+order by country;
